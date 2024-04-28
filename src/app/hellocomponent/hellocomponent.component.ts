6c2c90ea67db388c27bb6f906d722e8464169a52
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-28 16:39:47 -0300

    Exportando a classe do component

--- src/app/hellocomponent/hellocomponent.component.ts
+++ src/app/hellocomponent/hellocomponent.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './hellocomponent.component.html',
   styleUrl: './hellocomponent.component.css'
 })
+// Para expor a classe para outros arquivos -> export
 export class HellocomponentComponent {
 
 }
