77e25670b487a10aced0fe417f68d056a92469d8
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-28 16:35:46 -0300

    first commit

---
+++ src/app/hellocomponent/hellocomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hellocomponent',
+  templateUrl: './hellocomponent.component.html',
+  styleUrl: './hellocomponent.component.css'
+})
+export class HellocomponentComponent {
+
+}
